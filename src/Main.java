e4c72791de44c27d9401e3d773edfb7aa87055ca
Ildar <ildar.zackiev@yandex.ru>
2023-06-10 19:21:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 12300;
+        int bonus = 20;
+        int totalbonus = price / bonus;
+        System.out.println(totalbonus);
+    }
+}
